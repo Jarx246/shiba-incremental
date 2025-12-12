b7cc6eaff2d9a4e1a1f004fcf72a7d24fe4513ba
Jarx246 <51779056+Jarx246@users.noreply.github.com>
2025-12-13 02:33:05 +0800

    feat: implement stupidShibaLimit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import { ripple } from "@ripple-ts/vite-plugin";
-import { resolve } from "node:path";
+import { defineConfig } from 'vite';
+import { ripple } from '@ripple-ts/vite-plugin';
+import { resolve } from 'node:path';
 
 export default defineConfig({
-  base: "/shiba-incremental/",
-  publicDir: resolve(__dirname, "src/assets"),
-  plugins: [ripple()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-  },
+	base: '/shiba-incremental/',
+	publicDir: resolve(__dirname, 'src/assets'),
+	plugins: [ripple()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
 });
